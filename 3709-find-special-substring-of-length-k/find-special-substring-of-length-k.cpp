9c0b591470d852f89eea10f0e125cca921a85cdb
Nishant kumar <nishant.dev0112@gmail.com>
2026-06-29 13:56:49 -0400

    Time: 0 ms (100.00%) | Memory: 8.6 MB (60.61%) - LeetSync

---
+++ 3709-find-special-substring-of-length-k/find-special-substring-of-length-k.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool hasSpecialSubstring(string s, int k) {
+        int count = 1;
+        
+        for (int i = 1; i < s.length(); ++i) {
+            if (s[i] == s[i - 1]) {
+                count++;
+            } else {
+                if (count == k) {
+                    return true;
+                }
+                count = 1;
+            }
+        }
+        
+        return count == k;
+    }
+};
