d0400a6ec1dc9531541ecef15023d453fe71f046
Nishant kumar <nishant.dev0112@gmail.com>
2026-07-18 12:06:34 -0400

    Time: 0 ms (100.00%) | Memory: 8.6 MB (48.72%) - LeetSync

---
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        int count = 0;
+        if(n != 0){
+            count = n*n;
+            
+
+        }else{
+            count = n*(n+1);
+        }
+        return n;
+        
+    }
+};
